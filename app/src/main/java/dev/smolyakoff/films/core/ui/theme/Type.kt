d019780c47d6029ace19c5dd28081a36b407594b
incw <alexey.smolyakoff999@gmail.com>
2024-09-19 00:54:18 +0700

    Add typography

--- app/src/main/java/dev/smolyakoff/films/core/ui/theme/Type.kt
+++ app/src/main/java/dev/smolyakoff/films/core/ui/theme/Type.kt
@@ -1,5 +1,35 @@
 package dev.smolyakoff.films.core.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
 
-val Typography = Typography()
+val Typography = Typography(
+    titleLarge = TextStyle(
+        fontWeight = FontWeight(700),
+        fontSize = 20.sp,
+        lineHeight = 22.sp,
+        letterSpacing = 0.1.sp
+    ),
+    titleMedium = TextStyle(
+        fontSize = 16.sp,
+        fontWeight = FontWeight(400),
+        lineHeight = 20.sp,
+        letterSpacing = 0.1.sp
+    ),
+    headlineLarge = TextStyle(
+        fontWeight = FontWeight(500),
+        fontSize = 18.sp,
+        lineHeight = 22.sp,
+        letterSpacing = 0.15.sp,
+        textAlign = TextAlign.Center
+    ),
+    bodyLarge = TextStyle(
+        fontWeight = FontWeight(700),
+        fontSize = 16.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.1.sp
+    )
+)
