d935b67e3c1dfa42305f1aa7d3eea2a8ea5f1692
incw <alexey.smolyakoff999@gmail.com>
2024-09-20 23:00:55 +0700

    remove database code (comments)

--- app/src/main/java/dev/smolyakoff/films/features/films/storage/AppDatabase.kt
+++
@@ -1,9 +0,0 @@
-package dev.smolyakoff.films.features.films.storage
-
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-//@Database(entities = [FilmsDao::class], version = 1)
-//abstract class AppDatabase : RoomDatabase() {
-//    abstract fun filmsDao(): FilmsDao
-//}
